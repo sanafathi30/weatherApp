f87c09362ffa4e8d93d7e4dcd995e7d1add64789
sana <sana.fathi30@gmail.com>
2022-03-10 20:33:03 +0330

    forecast

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function formatData(timestemp) {
-    let days = [
+    let daysweek = [
         "Monday",
         "Tuesday",
         "Wednesday",
@@ -17,9 +17,64 @@ function formatData(timestemp) {
     if (h < 10) {
         h = `0${h}`;
     }
-    let d = days[data.getDay() - 1];
+    let d = daysweek[data.getDay() - 1];
 
     return `${d} ${h}:${m}`;
+}
+//
+function formatDay(timestemp) {
+    let dat = new Date(timestemp * 1000);
+    let day = dat.getDay();
+
+    let daysweek2 = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    return daysweek2[day];
+}
+//
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
+    // console.log(response.data.daily)
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHtml = `<div class="row">`;
+
+    forecast.forEach(function(forecastday, index) {
+        if (index < 6) {
+            forecastHtml =
+                forecastHtml +
+                `
+                        <div class="col-2">
+                            <div class="weather-forecast-date">${formatDay(
+                              forecastday.dt
+                            )}</div>
+
+                            <img src="http://openweathermap.org/img/wn/${
+                              forecastday.weather[0].icon
+                            }@2x.png" alt="" width="42px">
+                            <div class="weather-forecast-temprature">
+                                <span class="weather-forecast-temprature-maxi">${Math.round(
+                                  forecastday.temp.max
+                                )}°</span>
+                                <span class="weather-forecast-temprature-mini"> ${Math.round(
+                                  forecastday.temp.min
+                                )}°</span>
+                            </div>
+
+                        </div>
+
+                    `;
+        }
+    });
+
+    forecastHtml = forecastHtml + `</div>`;
+
+    forecastElement.innerHTML = forecastHtml;
+}
+//
+function getforecast(coordinates) {
+    let key = "0962dc1fc48ff43b087025aa1e35e11b";
+    let api = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
+    axios.get(api).then(displayForecast);
 }
 //
 function displayTempruture(response) {
@@ -49,6 +104,9 @@ function displayTempruture(response) {
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    //
+    getforecast(response.data.coord);
 }
 //
 function handleSubmit(event) {
@@ -93,6 +151,8 @@ c.addEventListener("click", showCelsTemprature);
 
 let cels = null;
 
+// displayForecast();
+
 let form = document.querySelector("#search-form");
 
 form.addEventListener("submit", handleSubmit);
@@ -103,4 +163,4 @@ form.addEventListener("keypress", (e) => {
     }
 });
 
-search("tehran");
+search("Tehran");
